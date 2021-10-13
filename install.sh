b330f291f7423ef8701bd27cdfc70e732723e838
Adrian <d.michael.adrian@gmail.com>
2021-10-13 08:42:05 +0200

    CRLF sh problem ( temp fix )

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
-APP=/grotrian-sensor
+APP=/grotrian-sensor;
 
-if [ -d "$APP" ]
-then
-    cd grotrian-sensor
-    git pull
+if[ -d "$APP" ]; then
+    cd grotrian-sensor;
+    git pull;
 else
-    git clone https://github.com/AlexanderADM/grotrian-sensor.git
-    cd grotrian-sensor
+    git clone https://github.com/AlexanderADM/grotrian-sensor.git;
+    cd grotrian-sensor;
 fi
 
-pip3 install -r requirements.txt
-python3 app.py
+pip3 install -r requirements.txt;
+python3 app.py;
