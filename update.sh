bebacd6b19aaa4d612c86b61c89466ddbe87a8db
Adrian <d.michael.adrian@gmail.com>
2021-10-14 08:21:35 +0200

    Fixed update script and verbosity

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+session="sensorInput"
 function UOI () {
 	git clone https://github.com/AlexanderADM/grotrian-sensor.git
 	shopt -s dotglob
@@ -11,17 +12,27 @@ function UOI () {
 function run () {
 	if [ ! -f "app.py" ]; then
 		echo "Python app not found, running update/install function."
-		sudo apt install tmux
 		UOI
-		tmux new -d -s sensorInput 'python3 app.py'
-	elif [[ "$(git status --porcelain --untracked-files=no)" ]]; then
+		echo "Finished installing the script."
+		tmux new -d -s $session 'python3 app.py'
+		echo "Script is now running in session \'sensorInput\'"
+	elif git status --branch --porcelain -uno | grep behind; then
 		echo "Differences from main branch found, updating script."
-		tmux kill-session -t sensorInput
+		echo "Terminating python script session."
+		tmux kill-session -t $session
 		UOI
-		tmux new -d -s sensorInput 'python3 app.py'
+		echo "Finished updating the script."
+		tmux new -d -s $session 'python3 app.py'
+		echo "Script is now running in session \'sensorInput\'."
 	else
 		echo "No differences found, no action taken."
+		tmux has-session -t $session 2>/dev/null
+		if [ $? != 0 ]; then
+			echo "The script is not running, rebooting the script."
+  			tmux new -d -s $session 'python3 app.py'
+			echo "Script is now running in session \'sensorInput\'."
+		fi
 	fi
 }
 
-while true; do run & sleep 60; done
+while true; do run & sleep 60m; done
